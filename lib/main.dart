d7152afe8a0240b929769837adacb8af5e38f26f
ikang-muse <inho.kang@muse.live>
2023-01-22 17:05:53 -0500

    use of Model and Future

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
+import 'package:toonflix/services/api_service.dart';
 
 void main() {
-  runApp(App());
+  ApiService().getTodaysToons();
+  runApp(const App());
 }
 
 class App extends StatefulWidget {
+  const App({super.key});
+
   @override
   State<App> createState() => _AppState();
 }
 
 class _AppState extends State<App> {
-  bool showTitle = true;
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(
-          textTheme: TextTheme(
-            titleLarge: TextStyle(
-              color: Colors.amber.shade900,
-            ),
-          ),
-        ),
-        home: Scaffold(
-          backgroundColor: const Color(0xFFF4EDDB),
-          body: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              showTitle ? const MyLargeTitle() : const Text('nothing'),
-              IconButton(
-                  onPressed: toggleTitle,
-                  icon: const Icon(Icons.remove_red_eye_rounded))
-            ]),
-          ),
-        ));
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    //initialize some data that depend on parent's data
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/services/api_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon.dart';
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var we in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromWebtoonJson(we));
+      }
+      return webtoonInstances;
+    }
+    throw Error();
+  }
+}
